a40e604783801b1a46e5fcc92e676845e16bc256
Joaquin <joaquinpatricio2013@gmail.com>
2021-04-18 05:21:05 -0300

    agregado lista de audios, solucionado crash en audios

--- index.js
+++ index.js
@@ -5,23 +5,17 @@ const client = new Discord.Client();
 const token = fs.readFileSync("./data/token.txt").toString();
 client.once("ready", () => {
 	console.log("MichaBot listo!");
-	client.user.setActivity('!help',{type: 'PLAYING'});
+	client.user.setActivity('!help', { type: 'PLAYING' });
 
 });
-
-
-
 
 //Carga data del bot (frases, etc)
 const frases = require('./data/JSON/frases.json');
 let help = "";
-fs.readFile('./data/help.txt', 'utf8', function(err, data) {
-    if (err) throw err;
+fs.readFile('./data/help.txt', 'utf8', function (err, data) {
+	if (err) throw err;
 	help = data;
 });
-
-
-
 
 
 
@@ -41,93 +35,120 @@ const getFrase = () => {//Elige frase random del JSON
 }
 
 
+
+
 var lastAudioInd = null; //guarda el ultimo audio elegido
-const getAudio = () => { //elige un audio random
+const getAudio = (n) => { //elige un audio random
 	let audios = [];
-	let randIndex;
-	fs.readdirSync('./data/sounds').forEach(File => {
+	fs.readdirSync('./data/sounds').forEach(File => { //carga la lista de audios
 		audios.push(File);
 	});
-	do {
-		randIndex = Math.floor(Math.random() * audios.length);
-	} while (randIndex == lastAudioInd)
-	lastAudioInd = randIndex;
-	console.log(randIndex);
-	let audio = `./data/sounds/${audios[randIndex]}`;
-	return audio;
+	if (n == "lista") {
+		return audios; //devuelve la lista de audios
+	}
+	else if (n == "random") {
+		let randIndex;
+		do {
+			randIndex = Math.floor(Math.random() * audios.length);
+		} while (randIndex == lastAudioInd)
+		lastAudioInd = randIndex;
+		console.log(randIndex);
+		let audio = `./data/sounds/${audios[randIndex]}`;
+		return audio; //devuelve audio random
+	}
+	else {
+		let audio = `./data/sounds/${audios[n]}`;
+		return audio; //devuelve el audio por index
+	}
+
 }
 
 
 
 
 
-
-
-
-//comandos del bot
 client.on('message', msg => {
+	if (msg.content === '!help') {
+		msg.channel.send(`${help}`);
+	}
+
 	if (msg.content === 'hola micha') {
 		msg.channel.send('jajaja en que andan manga de gays?');
 	}
-});
 
-client.on('message', msg => {
 	if (msg.content === 'kufa') {
 		msg.channel.send('ese es el verdadero mercenario🔪🔪');
 	}
-});
 
-client.on('message', msg => {
 	if (msg.content === '!frase') {
 		msg.channel.send(getFrase());
 	}
-});
 
 
-
-client.on('message', msg => {
-	if (msg.content === '!audio') {
-		const voiceChannel = msg.member.voice.channel;
-		try {
-			voiceChannel.join().then(connection => {
-				const dispatcher = connection.play(`${getAudio()}`);
-				dispatcher.on("end", end => {
-					voiceChannel.leave();
-				});
-				client.on('message', msg => {
-					if (msg.content === '!chau') {
-						voiceChannel.leave();
-					}
-
+	if (msg.content.startsWith("!audio")) {
+		const prefix = "!audio"
+		const args = msg.content.slice(prefix.length).trim().split(' ')
+		let audio = "";
+		if (args.length == 1 && args[0] != '') {
+			if (args[0] == "lista") {//si el argumento es "lista"
+				let lista = getAudio("lista")
+				let listaMsg = "```Elegir audio con !audio **numero**"
+				lista.forEach((file, index) => {
+					listaMsg = listaMsg.concat(`\n${index}: ${file}`)
 				})
-			}).catch(err => console.log(err));
+				listaMsg = listaMsg.concat("```")
+				msg.channel.send(listaMsg);
+			}
+			else if (args[0].match(/^[0-9]+$/) != null) {//si es un numero
+				audio = getAudio(args[0])
+			}
+			else if (args[0].match(/^[0-9]+$/) == null) { //si no es lista ni numero
+				msg.channel.send("??? que re carajos digooo")
+			}
 		}
-		catch {
-			msg.channel.send("TENES QUE ESTAR EN UN CHAT DE VOZ PANCHO")
+		else if (args.length == 1 && args[0] == '') {
+			audio = getAudio("random")
+		}
+		else { //si tiene mas de un argumento
+			msg.channel.send("??? que re carajos digooo")
 		}
 
 
-	}
-});
 
 
+		if (audio != "") {
+			const voiceChannel = msg.member.voice.channel;
+			if (voiceChannel) { //si usuario en canal de voz
+				voiceChannel.join().then(connection => {
+					const dispatcher = connection.play(`${audio}`);
+					client.on('message', msg => {
+						if (msg.content === '!chau') {
+							voiceChannel.leave();
+						}
+					})
+				}).catch(err => console.log(err));
+			}
+			else {
+				msg.channel.send("TENES QUE ESTAR EN UN CHAT DE VOZ PANCHO")
+			}
 
-client.on('message', msg =>{
-	if(msg.content === '!help'){
-		msg.channel.send(`${help}`);
+
+		}
 	}
 })
 
-
-
-
-
-
-
-
-
-
-
-
 client.login(token);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
